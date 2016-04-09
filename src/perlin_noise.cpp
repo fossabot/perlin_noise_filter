038fad7df7e9178bdcabd2ec41cd67f88a81dfc5
Jake Dexheimer <jakedex@gmail.com>
2016-04-09 18:13:50 -0500

    Actually regestering now

--- src/perlin_noise.cpp
+++ src/perlin_noise.cpp
@@ -1,5 +1,6 @@
 #include "perlin_noise_filter/perlin_noise.h"
 #include "pluginlib/class_list_macros.h"
+
 using namespace perlin_noise_filters;
 PerlinNoiseFilter::PerlinNoiseFilter()
 {}
@@ -8,7 +9,8 @@ PerlinNoiseFilter::~PerlinNoiseFilter()
 {}
 
 bool PerlinNoiseFilter::configure()
-{   return true;}
+{   return true;
+}
 
 bool PerlinNoiseFilter::update(const double & input, double & output)
 {
@@ -16,4 +18,4 @@ bool PerlinNoiseFilter::update(const double & input, double & output)
     return true;
 };
 
-PLUGINLIB_REGISTER_CLASS(PerlinNoiseResult, perlin_noise_filters::PerlinNoiseFilter, filters::FilterBase<double>)
+PLUGINLIB_REGISTER_CLASS(PerlinNoiseDouble, perlin_noise_filters::PerlinNoiseFilter, filters::FilterBase<double>)
